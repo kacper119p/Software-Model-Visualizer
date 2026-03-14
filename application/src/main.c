b6ceea666c467407a34985c27bbcaec4318dfd2c
Kacper Pastuszka <91415004+kacper119p@users.noreply.github.com>
2026-03-14 15:36:13 +0100

    Update main.c

--- application/src/main.c
+++ application/src/main.c
@@ -8,9 +8,6 @@
 int main(void) {
   while (true) {
     int32_t width, height;
-    getConsolePixelSize(&width, &height);
-    Framebuffer fb;
-    framebufferInit(&fb, width, height);
     printf("width = %d, height = %d\n", width, height);
   }
   return 0;
