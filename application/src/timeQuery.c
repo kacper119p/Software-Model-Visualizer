59594615c6ed87d344e5fab96e3f91240894faaf
Kacper Pastuszka <91415004+kacper119p@users.noreply.github.com>
2026-03-15 09:38:25 +0100

    Added time query

---
+++ application/src/timeQuery.c
@@ -0,0 +1,15 @@
+#include "timeQuery.h"
+
+#include <profileapi.h>
+
+void initializeTimeQuery(TimeQuery* TimeQuery) {
+  QueryPerformanceFrequency(&TimeQuery->Frequency);
+  QueryPerformanceCounter(&TimeQuery->Start);
+}
+float getElapsedTime(const TimeQuery* const TimeQuery) {
+  LARGE_INTEGER end;
+  QueryPerformanceCounter(&end);
+
+  return (float)(end.QuadPart - TimeQuery->Start.QuadPart) /
+         (float)TimeQuery->Frequency.QuadPart;
+}
