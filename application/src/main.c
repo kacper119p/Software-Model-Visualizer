ad0d26565b6e52d0a5b1d3301df2bff1d72f2472
Kacper Pastuszka <91415004+kacper119p@users.noreply.github.com>
2026-03-19 12:28:23 +0100

    Added projection matrix

--- application/src/main.c
+++ application/src/main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 #include "cglm/affine.h"
-#include "cglm/mat4.h"
+#include "cglm/cam.h"
 #include "rendering.h"
 #include "timeQuery.h"
 #include "window.h"
@@ -35,13 +35,19 @@ int main(int argc, char* argv[]) {
     peekWindowMessages(window);
     clearColorBuffer(&window->Framebuffer, 0x00000000);
     clearDepthBuffer(&window->Framebuffer, 1.0f);
+    mat4 projectionMatrix;
+    const float aspect =
+        (float)window->Framebuffer.Width / (float)window->Framebuffer.Height;
+    glm_perspective(90.0f, aspect, 0.1f, 100.0f, projectionMatrix);
     mat4 transform;
     vec3 translation = {0.5f, 0.75f, 0.0f};
     glm_translate_make(transform, translation);
     vec3 scale = {0.18f, -0.18f, 0.18f};
     glm_scale(transform, scale);
     glm_rotate(transform, currentTime, (vec3){0.0f, 1.0f, 0.0f});
-    drawModel(&window->Framebuffer, &model, transform);
+    mat4 finalTransform;
+    glm_mat4_mul(projectionMatrix, transform, finalTransform);
+    drawModel(&window->Framebuffer, &model, finalTransform);
     presentWindow(window);
   }
 
