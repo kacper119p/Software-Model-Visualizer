fe29f96483cdd04269e59878266dc2d86b2c525b
Kacper Pastuszka <91415004+kacper119p@users.noreply.github.com>
2026-04-08 12:53:44 +0200

    Update testFramework.h

--- testFramework/inc/testFramework.h
+++ testFramework/inc/testFramework.h
@@ -35,8 +35,8 @@ struct TestEntry {
 
 #define _ASSERT_EQUAL_INT_(Actual, Expected, Type, Format)                     \
   do {                                                                         \
-    const Type _actual_ = (Actual);                                            \
-    const Type _expected_ = (Expected);                                        \
+    const Type _actual_ = Actual;                                              \
+    const Type _expected_ = Expected;                                          \
     if (_actual_ != _expected_) {                                              \
       _Test_Data_->TestResult = TestResult_Failure;                            \
       printf(ANSI_RED "ASSERTION FAILED" ANSI_RESET ": %s:%d: "                \
@@ -82,6 +82,69 @@ struct TestEntry {
   _ASSERT_CLOSE_FLOAT_(Actual, Expected, Tolerance, float, "%f")
 #define ASSERT_CLOSE_DOUBLE(Actual, Expected, Tolerance)                       \
   _ASSERT_CLOSE_FLOAT_(Actual, Expected, Tolerance, double, "%lf")
+
+#define _ASSERT_COMPARE_(Actual, Expected, Type, Format, Comparator,           \
+                         ComparisonMessage)                                    \
+  do {                                                                         \
+    const Type _actual_ = Actual;                                              \
+    const Type _expected_ = Expected;                                          \
+    if (!(_actual_ Comparator _expected_)) {                                   \
+      _Test_Data_->TestResult = TestResult_Failure;                            \
+      printf(ANSI_RED "ASSERTION FAILED" ANSI_RESET ": %s:%d: "                \
+                      "Expected " Format " to be " ComparisonMessage Format    \
+                      ".\n",                                                   \
+             __FILE__, __LINE__, _expected_, _actual_);                        \
+      return;                                                                  \
+    }                                                                          \
+  } while (0)
+
+#define _ASSERT_LESS_(Actual, Expected, Type, Format)                          \
+  _ASSERT_COMPARE_(Actual, Expected, Type, Format, <, "less than ")
+
+#define ASSERT_LESS_INT8(Actual, Expected)                                     \
+  _ASSERT_LESS_(Actual, Expected, int8_t, "%" PRId8)
+#define ASSERT_LESS_INT16(Actual, Expected)                                    \
+  _ASSERT_LESS_(Actual, Expected, int16_t, "%" PRId16)
+#define ASSERT_LESS_INT32(Actual, Expected)                                    \
+  _ASSERT_LESS_(Actual, Expected, int32_t, "%" PRId32)
+#define ASSERT_LESS_INT64(Actual, Expected)                                    \
+  _ASSERT_LESS_(Actual, Expected, int64_t, "%" PRId64)
+#define ASSERT_LESS_UINT8(Actual, Expected)                                    \
+  _ASSERT_LESS_(Actual, Expected, uint8_t, "%" PRIu8)
+#define ASSERT_LESS_UINT16(Actual, Expected)                                   \
+  _ASSERT_LESS_(Actual, Expected, uint16_t, "%" PRIu16)
+#define ASSERT_LESS_UINT32(Actual, Expected)                                   \
+  _ASSERT_LESS_(Actual, Expected, uint32_t, "%" PRIu32)
+#define ASSERT_LESS_UINT64(Actual, Expected)                                   \
+  _ASSERT_LESS_(Actual, Expected, uint64_t, "%" PRIu64)
+#define ASSERT_LESS_FLOAT(Actual, Expected)                                    \
+  _ASSERT_LESS_(Actual, Expected, float, "%f")
+#define ASSERT_LESS_DOUBLE(Actual, Expected)                                   \
+  _ASSERT_LESS_(Actual, Expected, double, "%lf")
+
+#define _ASSERT_GREATER_(Actual, Expected, Type, Format)                       \
+  _ASSERT_COMPARE_(Actual, Expected, Type, Format, >, "greater than ")
+
+#define ASSERT_GREATER_INT8(Actual, Expected)                                  \
+  _ASSERT_GREATER_(Actual, Expected, int8_t, "%" PRId8)
+#define ASSERT_GREATER_INT16(Actual, Expected)                                 \
+  _ASSERT_GREATER_(Actual, Expected, int16_t, "%" PRId16)
+#define ASSERT_GREATER_INT32(Actual, Expected)                                 \
+  _ASSERT_GREATER_(Actual, Expected, int32_t, "%" PRId32)
+#define ASSERT_GREATER_INT64(Actual, Expected)                                 \
+  _ASSERT_GREATER_(Actual, Expected, int64_t, "%" PRId64)
+#define ASSERT_GREATER_UINT8(Actual, Expected)                                 \
+  _ASSERT_GREATER_(Actual, Expected, uint8_t, "%" PRIu8)
+#define ASSERT_GREATER_UINT16(Actual, Expected)                                \
+  _ASSERT_GREATER_(Actual, Expected, uint16_t, "%" PRIu16)
+#define ASSERT_GREATER_UINT32(Actual, Expected)                                \
+  _ASSERT_GREATER_(Actual, Expected, uint32_t, "%" PRIu32)
+#define ASSERT_GREATER_UINT64(Actual, Expected)                                \
+  _ASSERT_GREATER_(Actual, Expected, uint64_t, "%" PRIu64)
+#define ASSERT_GREATER_FLOAT(Actual, Expected)                                 \
+  _ASSERT_GREATER_(Actual, Expected, float, "%f")
+#define ASSERT_GREATER_DOUBLE(Actual, Expected)                                \
+  _ASSERT_GREATER_(Actual, Expected, double, "%lf")
 
 #define TEST_PREFIX_(a, b) a##__##b
 #define TEST_PREFIX(a, b) TEST_PREFIX_(a, b)
