808b3606dd122304db2e89fa4b35c10ef7c5e697
Kacper Pastuszka <91415004+kacper119p@users.noreply.github.com>
2026-04-14 01:01:02 +0200

    Update main.c

--- application/src/main.c
+++ application/src/main.c
@@ -4,31 +4,14 @@
 
 #include "appWindow.h"
 #include "rendering.h"
-#include "timeQuery.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(const int argc, char* argv[]) {
-  if (argc != 2) {
-    fprintf(stderr,
-            "Error: Invalid number of arguments.\n"
-            "Usage: %s <file_path>\n",
-            (argc > 0) ? argv[0] : "rasterizer");
-    return EXIT_FAILURE;
-  }
-
-  Model model;
-  vec3 modelCenter;
-  float extent;
-  loadModel(argv[1], &model, &modelCenter, &extent);
-
+int main(void) {
   srand(time(nullptr));
 
   AppWindow* window = createWindow();
-
-  TimeQuery timeQuery;
-  initializeTimeQuery(&timeQuery);
 
   while (!window->ShouldClose) {
 
@@ -41,7 +24,6 @@ int main(const int argc, char* argv[]) {
     presentWindow(window);
   }
 
-  destroyModel(&model);
   destroyWindow(&window);
   return EXIT_SUCCESS;
 }
